cbc72afb4432babd9e854c1bac3d147736096bb5
aalltt <geekjjssww@list.ru>
2019-05-28 23:25:54 +0300

    replace

--- GeekLessons/lesson1_6/AnimalAction.java
+++
@@ -1,7 +0,0 @@
-package lesson1_6;
-
-public interface AnimalAction {
-    boolean Run(int dlina_zabega_i);
-    boolean Jump(float visota_prizka_i);
-    boolean Swim(int dlina_zapliva_i);
-}
--- GeekLessons/lesson1_6/Dog.java
+++
@@ -1,9 +0,0 @@
-package lesson1_6;
-
-public class Dog extends Animal {
-
-
-    public Dog(String name, int dlina_zabega, float visota_prizka, int dlina_zapliva) {
-        super(name, dlina_zabega, visota_prizka, dlina_zapliva);
-    }
-}
