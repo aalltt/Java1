1797a942be906444163948216cc5aa0a0c8a3d12
aalltt <geekjjssww@list.ru>
2019-06-20 22:38:53 +0300

    Дано натуральное число N. Вычислите сумму его цифр

---
+++ Recursion/SummaRec.java
@@ -0,0 +1,17 @@
+﻿package Recursion;
+/* Дано натуральное число N. Вычислите сумму его цифр*/
+
+public class SummaRec {
+    static int getRec(int n) {
+       int result;
+        if (n == 0)
+            return 0;
+       result=n+getRec(n-1);
+       return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getRec(6));
+    }
+}
+
