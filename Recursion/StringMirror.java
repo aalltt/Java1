0975ce769e9f2c2632be15954bffefbb2b18abca
aalltt <geekjjssww@list.ru>
2019-06-20 22:38:29 +0300

    Вывести строку в обратном порядке

---
+++ Recursion/StringMirror.java
@@ -0,0 +1,23 @@
+﻿package Recursion;
+/*Вывести строку в обратном порядке*/
+
+class StringMirror {
+    String tt;
+
+    public StringMirror(String tt) {
+        this.tt = tt;
+    }
+
+    void Mirror(int i) {
+        if (i != tt.length() - 1)
+            Mirror(i + 1);
+        System.out.print(tt.charAt(i));
+    }
+}
+
+class StringMirrorMain {
+    public static void main(String[] args) {
+        StringMirror stm = new StringMirror("qwerty");
+        stm.Mirror(3);
+    }
+}
